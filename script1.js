1e81eb7d38b9fbb42aa4f461e9f553efc03e3ce9
Leorr22 <133364916+Leorr22@users.noreply.github.com>
2024-05-05 21:02:00 +0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,17 @@
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+var image = document.querySelector('#contacts > .contA > a > img');
+
+image.addEventListener('click', function() {
+    image.classList.toggle('enlarged');
+});
+
